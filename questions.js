6f5de2ca0001404dfabca072e079f63f164b02a9
Aimee Millard <an.millard19@gmail.com>
2020-06-14 15:40:24 -0400

    complete and functional

---
+++ questions.js
@@ -0,0 +1,72 @@
+// //array of questions
+// var questions = [
+//   {
+//     question:
+//       "The condition in an if / else statement is enclosed within ____.",
+//     choices: [
+//       "a: quotes",
+//       "b: curley brackets",
+//       "c: square brackets",
+//       "d: parentheses",
+//     ],
+//     answer: "d",
+//   },
+//   {
+//     question: "Which symbol represent 'or'?",
+//     choices: ["a: &&", "b: ===", "c: ||", "d: OR"],
+//     answer: "c",
+//   },
+//   {
+//     question: "The less than/or equal operator is _____.",
+//     choices: ["a: <=", "b: >=", "c: +=", "d: =="],
+//     answer: "b",
+//   },
+//   {
+//     question: "An array is enclosed in ______.",
+//     choices: [
+//       "a: quotes",
+//       "b: parentheses",
+//       "c: curley brackets",
+//       "d: square brackets",
+//     ],
+//     answer: "d",
+//   },
+//   {
+//     question: "What doe DOM stand for?",
+//     choices: [
+//       "a: Desktop Oriented Model",
+//       "b: Display Object Model",
+//       "c: Document Object Model",
+//       "d: Digital Oriented Model",
+//     ],
+//     answer: "b",
+//   },
+// ];
+
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Most widely used coding language for web development:",
+    choices: ["C#", "javascript", "python", "ruby"],
+    answer: "javascript",
+  },
+  {
+    title: 'The math operator "%" is used to calculate what?',
+    choices: ["product", "remainder", "sum", "quotient"],
+    answer: "remainder",
+  },
+  {
+    title: "What language does not use curly brackets like { and }?",
+    choices: ["java", "objective-c", "C#", "python"],
+    answer: "python",
+  },
+];
